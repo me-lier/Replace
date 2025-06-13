398c9007c88eff01b8a9eb5ec04271a522ab605d
Srinivas Deekonda <srinivaslier@gmail.com>
2025-06-13 22:56:35 +0530

    Deploying testing

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -47,21 +47,6 @@ export {
     signInWithPhoneNumber
 };
 
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBHepnJbS30yYzZkR1ZyQ1yF1eVKOEDbZo",
-    authDomain: "replace-8af45.firebaseapp.com",
-    databaseURL: "https://replace-8af45-default-rtdb.firebaseio.com",
-    projectId: "replace-8af45",
-    storageBucket: "replace-8af45.firebasestorage.app",
-    messagingSenderId: "228567518315",
-    appId: "1:228567518315:web:9b620686bd70d5afb4f52e",
-    measurementId: "G-7K6W5KETK2"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 // Function to save chatbot data
 async function saveChatbotData(data) {
     try {
